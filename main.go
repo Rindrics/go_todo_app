c72a7de2b21cf4d12618d75e58cc08f92bc7b887
Akira Hayashi <smxshxishxad@me.com>
2022-08-13 02:19:49 +0900

    :sparkles: Handle signal

--- main.go
+++ main.go
@@ -6,12 +6,18 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Rindrics/go_todo_app/config"
 	"golang.org/x/sync/errgroup"
 )
 
 func run(ctx context.Context) error {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -24,6 +30,7 @@ func run(ctx context.Context) error {
 	log.Printf("start with: %v", url)
 	s := &http.Server{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(5 * time.Second)
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
