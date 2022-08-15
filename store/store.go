f52bc1a27815d103a05dde5b1dc497c1604a6f35
Akira Hayashi <smxshxishxad@me.com>
2022-08-15 18:20:31 +0900

    :sparkles: Define `TaskStore` to add and check tasks

---
+++ store/store.go
@@ -0,0 +1,26 @@
+package store
+
+import (
+	"github.com/Rindrics/go_todo_app/entity"
+)
+
+type TaskStore struct {
+	// intendedly exported to check behavior
+	LastID entity.TaskID
+	Tasks  map[entity.TaskID]*entity.Task
+}
+
+func (ts *TaskStore) Add(t *entity.Task) (entity.TaskID, error) {
+	ts.LastID++
+	t.ID = ts.LastID
+	ts.Tasks[t.ID] = t
+	return t.ID, nil
+}
+
+func (ts *TaskStore) All() entity.Tasks {
+	tasks := make([]*entity.Task, len(ts.Tasks))
+	for i, t := range ts.Tasks {
+		tasks[i-1] = t
+	}
+	return tasks
+}
