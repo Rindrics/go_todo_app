e4ad32730f057a673c6bf4aab6d8949403b73bd5
Akira Hayashi <smxshxishxad@me.com>
2022-08-19 07:46:17 +0900

    :recycle: Receive interface `Queryer` instead of `*sql.DB`

--- store/task.go
+++ store/task.go
@@ -2,33 +2,20 @@ package store
 
 import (
 	"context"
-	"database/sql"
 
 	"github.com/Rindrics/go_todo_app/entity"
 )
 
 func (r *Repository) ListTasks(
-	ctx context.Context, db *sql.DB,
+	ctx context.Context, db Queryer,
 ) (entity.Tasks, error) {
+	tasks := entity.Tasks{}
 	sql := `SELECT
               id, title,
               status, created, modified
             FROM task;`
-	rows, err := db.QueryContext(ctx, sql)
-	if err != nil {
+	if err := db.SelectContext(ctx, &tasks, sql); err != nil {
 		return nil, err
-	}
-	defer rows.Close()
-	var tasks entity.Tasks
-	for rows.Next() {
-		t := &entity.Task{}
-		if err := rows.Scan(
-			&(t.ID), &(t.Title),
-			&(t.Status), &(t.Created), &(t.Modified),
-		); err != nil {
-			return nil, err
-		}
-		tasks = append(tasks, t)
 	}
 	return tasks, nil
 }
