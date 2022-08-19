35a8007ddd0c1645ce9a542856312efc30f42793
Akira Hayashi <smxshxishxad@me.com>
2022-08-19 09:50:51 +0900

    :green_heart: Test `AddTask()`

--- store/task_test.go
+++ store/task_test.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/Rindrics/go_todo_app/clock"
 	"github.com/Rindrics/go_todo_app/entity"
 	"github.com/Rindrics/go_todo_app/testutil"
 	"github.com/google/go-cmp/cmp"
+	"github.com/jmoiron/sqlx"
 )
 
 func TestRepository_ListTasks(t *testing.T) {
@@ -71,4 +73,34 @@ func prepareTasks(ctx context.Context, t *testing.T, con Execer) entity.Tasks {
 	wants[1].ID = entity.TaskID(id + 1)
 	wants[2].ID = entity.TaskID(id + 2)
 	return wants
+}
+
+func TestRepository_AddTask(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+
+	c := clock.FixedClocker{}
+	var wantID int64 = 1
+	okTask := &entity.Task{
+		Title:    "task title",
+		Status:   entity.TaskStatusTodo,
+		Created:  c.Now(),
+		Modified: c.Now(),
+	}
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() { db.Close() })
+	mock.ExpectExec(
+		`INSERT INTO task \(title, status, created, modified\) VALUES \(\?, \?, \?, \?\)`,
+	).WithArgs(okTask.Title, okTask.Status, okTask.Created, okTask.Modified).
+		WillReturnResult(sqlmock.NewResult(wantID, 1))
+
+	xdb := sqlx.NewDb(db, "mysql")
+	r := &Repository{Clocker: c}
+	if err := r.AddTask(ctx, xdb, okTask); err != nil {
+		t.Errorf("want no error, but got %v", err)
+	}
 }
