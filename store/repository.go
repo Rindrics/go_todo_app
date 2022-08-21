66f1f57db7d792412807b49cfac37637acf2c38e
Akira Hayashi <smxshxishxad@me.com>
2022-08-22 03:49:24 +0900

    :sparkles: Add `RegisterUser()` in package `store`

--- store/repository.go
+++ store/repository.go
@@ -3,6 +3,7 @@ package store
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"time"
 
@@ -66,3 +67,11 @@ var (
 type Repository struct {
 	Clocker clock.Clocker
 }
+
+const (
+	ErrCodeMySQLDuplicateEntry = 1062
+)
+
+var (
+	ErrAlreadyEntry = errors.New("duplicate entry")
+)
